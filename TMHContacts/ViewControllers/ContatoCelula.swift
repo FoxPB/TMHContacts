c61bebbe0f946a53316afc805af279f9ddd6ebe5
Ricardo Caldeira de Andrade <ricardo.caldeira3@hotmail.com>
2019-07-18 13:22:49 -0300

    Esqueci desses arquivos

---
+++ TMHContacts/ViewControllers/ContatoCelula.swift
@@ -0,0 +1,18 @@
+//
+//  ContatoCelula.swift
+//  TMHContacts
+//
+//  Created by Ricardo Caldeira on 15/07/19.
+//  Copyright © 2019 Ricardo Caldeira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContatoCelula: UITableViewCell {
+    
+    @IBOutlet weak var imageContato: UIImageView!
+    @IBOutlet weak var nameContato: UILabel!
+    @IBOutlet weak var alertaContato: UILabel!
+    
+}
